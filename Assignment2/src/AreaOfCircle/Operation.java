5978f96c5ab05899602f936d20f050af46838500
k-d@r <kedar.sedai05@gmail.com>
2021-03-08 20:02:07 +0545

    Added mathod suggestion

--- Assignment2/src/AreaOfCircle/Operation.java
+++ Assignment2/src/AreaOfCircle/Operation.java
@@ -6,7 +6,7 @@ public class Operation {
     public void Operation(){
         System.out.println("enter your radius");
         Scanner aka = new Scanner(System.in);
-        double radius = aka.nextFloat();
+        double radius = aka.nextFloat(); // you can use nextDouble() method in Java
         double Area = Math.PI * Math.pow(radius,2);
         System.out.println("the area of circle with radius "+radius+" is "+Area);
     }
